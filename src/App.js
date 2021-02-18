9e752e8b51c86be2d83620c34d1cfc41aa0ced82
Atif Qayyum <aqarain92@gmail.com>
2021-02-18 21:33:33 +0500

    About Section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,7 @@
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
