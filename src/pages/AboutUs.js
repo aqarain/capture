2ddc026bb73ff64432ddb65fad41669f47110b81
Atif Qayyum <aqarain92@gmail.com>
2021-02-18 21:47:00 +0500

    Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,13 @@
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
