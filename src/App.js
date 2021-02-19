6ed6ec0c414c8cc49a86677c90300dbd6f93e812
Atif Qayyum <aqarain92@gmail.com>
2021-02-19 22:43:11 +0500

    Nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
