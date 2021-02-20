09d20863515f256b31a6a79070745a616ccdda3f
Atif Qayyum <aqarain92@gmail.com>
2021-02-20 14:54:07 +0500

    Wave added in AboutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
 import { motion } from "framer-motion";
 import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -29,6 +30,7 @@ const AboutSection = () => {
       <Image>
         <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
